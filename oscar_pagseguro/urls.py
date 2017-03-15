2c15714dd9a6f7f60c841e15785249b64279a205
Caio Marinho <caiomarinho8@gmail.com>
2017-03-14 22:57:38 -0300

    Update urls.py

--- oscar_pagseguro/urls.py
+++ oscar_pagseguro/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import *
 
 from .views import SuccessResponseView
 
-urlpatterns = patterns('',
+urlpatterns = ('',
     url(r'^preview/(?P<basket_id>\d+)/$', SuccessResponseView.as_view(preview=True), name='pagseguro-success-response'),
     url(r'^checkout/payment-details/$', SuccessResponseView.as_view(preview=True), name='pagseguro-success-response'),
     url(r'^checkout/preview/$', SuccessResponseView.as_view(preview=True), name='pagseguro-success-response'),
